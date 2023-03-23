98534ffe36bf0263c2fa30e8dfac400cbefc2448
Charles <cthomas@callistomedia.com>
2023-03-23 15:56:21 -0700

    Add common config

---
+++ src/main/java/com/rumpus/common/CommonConfig.java
@@ -0,0 +1,64 @@
+package com.rumpus.common;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+/**
+ * @author Chuck Thomas
+ * 
+ * Common config for web app. Using jdbc template right now. Should abstract this to allow other impls.
+ * TODO: think about making this into an annotation.
+ */
+public abstract class CommonConfig extends Rumpus {
+
+    @Autowired
+	protected Environment environment;
+
+    protected final String URL = "url";
+	protected final String USER = "username";
+	protected final String DRIVER = "driver";
+	protected final String PASSWORD = "password";
+
+    @Bean
+	protected DataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setUrl(environment.getProperty(URL));
+		driverManagerDataSource.setUsername(environment.getProperty(USER));
+		driverManagerDataSource.setPassword(environment.getProperty(PASSWORD));
+		driverManagerDataSource.setDriverClassName(environment.getProperty(DRIVER));
+		return driverManagerDataSource;
+	}
+
+    @Bean
+	public JdbcUserDetailsManager jdbcUserDetailsManager() {
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+
+		jdbcUserDetailsManager.setDataSource(dataSource());
+        return jdbcUserDetailsManager;
+
+        // CommonJdbcUserManager manager = new CommonJdbcUserManager(dataSource);
+        // manager.manager().setUsersByUsernameQuery(SET_USERS_QUERY);
+		// return manager.manager();
+	}
+    
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(jdbcUserDetailsManager());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
