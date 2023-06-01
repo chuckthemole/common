5936696c68264d76cdc2e43780a49125d3cc00be
Chuck Thomas <chuckthemole@gmail.com>
2023-06-01 16:57:04 -0700

    Add ActivateModalNoButton to js

--- src/main/js/common.js
+++ src/main/js/common.js
@@ -43,3 +43,29 @@ export function ActivateModal(modal, button) {
        });
    }
 }
+
+// Activates modal without button
+export function ActivateModalNoButton(modal) {
+    modal.classList.add("is-active");
+    modal.classList.add("is-clipped");
+   if (modal.getElementsByClassName("modal-close")[0] !== undefined) {
+       modal.getElementsByClassName("modal-close")[0].onclick = function () {
+           modal.classList.remove("is-active");
+           modal.classList.remove("is-clipped");
+       }
+   }
+
+   // If click outside the modal, close modal
+   window.onclick = function (event) {
+       var backgrounds = document.getElementsByClassName("modal-background");
+       Array.prototype.forEach.call(backgrounds, function (background) { // get all modal backgrounds
+           if (event.target == background) {
+               var modals = document.getElementsByClassName("modal");
+               Array.prototype.forEach.call(modals, function (modal) { // remove is-active from all modals
+                   modal.classList.remove("is-active");
+                   modal.classList.remove("is-clipped");
+               });
+           }
+       });
+   }
+}
