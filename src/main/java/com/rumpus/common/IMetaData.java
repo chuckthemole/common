1fb5b0b7f995f6926c2f2cc9edfa902d1f96a49f
Chuck Thomas <chuckthemole@gmail.com>
2023-06-26 15:08:02 -0700

    Clean up MetaData class

--- src/main/java/com/rumpus/common/IMetaData.java
+++ src/main/java/com/rumpus/common/IMetaData.java
@@ -8,12 +8,7 @@ import org.springframework.jdbc.core.support.SqlLobValue;
 import com.google.gson.TypeAdapter;
 
 public interface IMetaData<MODEL extends Model<MODEL>> extends IRumpusObject {
-    // byte[] getData();
-    // void setData(byte[] data);
     Instant getCreationTime();
     void setCreationTime(Instant time);
     String getStandardFormattedCreationTime();
-    // SqlLobValue getLobValue();
-    // void setTypeAdapter(TypeAdapter<?> typeAdapter);
-    // TypeAdapter<?> getTypeAdapter();
 }
--- src/main/java/com/rumpus/common/MetaData.java
+++ src/main/java/com/rumpus/common/MetaData.java
@@ -23,29 +23,13 @@ import com.google.gson.TypeAdapter;
  */
 public abstract class MetaData<MODEL extends Model<MODEL>> extends Model<MODEL> implements IMetaData<MODEL> {
 
-    // private byte[] data = null;
     private Instant creationTime;
     public static final String USER_CREATION_DATE_TIME = "user_creation_datetime";
-    // private TypeAdapter<?> typeAdapter = null;
 
     public MetaData(String name) {
         super(name);
         this.creationTime = Instant.now();
     }
-
-    // @Override
-    // public void setData(byte[] data) {
-    //     this.data = data;
-    // }
-
-    // @Override
-    // public byte[] getData() {
-    //     if(this.data == null) {
-    //         LOG.info("Error: Cannot get data because it has not been set.");
-    //         return new byte[0]; // TODO: returning empty for now. Maybe return null
-    //     }
-    //     return this.data;
-    // }
 
     @Override
     public void setCreationTime(Instant creationTime) {
@@ -57,28 +41,6 @@ public abstract class MetaData<MODEL extends Model<MODEL>> extends Model<MODEL> 
         return this.creationTime;
     }
 
-    // @Override
-    // public void setTypeAdapter(TypeAdapter<?> typeAdapter) {
-    //     this.typeAdapter = typeAdapter;
-    // }
-
-    // @Override
-    // public TypeAdapter<?> getTypeAdapter() {
-    //     if(this.typeAdapter != null) {
-    //         return this.typeAdapter;
-    //     }
-    //     LOG.info("Error: TypeAdapter is not set. Returning nulll...");
-    //     return null;
-    // }
-
-    // @Override
-    // public SqlLobValue getLobValue() {
-    //     ByteArrayInputStream inputStrm = new ByteArrayInputStream(data);
-    //     LobHandler handler = new DefaultLobHandler();
-    //     SqlLobValue sqlLobValue = new SqlLobValue(inputStrm, data.length, handler);
-    //     return sqlLobValue;
-    // }
-
     /**
      * 
      * @return LocalDateTime object to string using ZoneOffset.UTC <- TODO look into this more. may have to come up with standards for time.
@@ -87,10 +49,4 @@ public abstract class MetaData<MODEL extends Model<MODEL>> extends Model<MODEL> 
     public final String getStandardFormattedCreationTime() {
         return LocalDateTime.ofInstant(this.creationTime, ZoneOffset.UTC).toString();
     }
-
-    // public void test() {
-    //     SqlLobValue v = new SqlLobValue(new byte[2]);
-    //     MapSqlParameterSource in = new MapSqlParameterSource();
-    //     in.addValue("image",  new SqlLobValue(new ByteArrayInputStream(data), data.length, new DefaultLobHandler()), Types.BLOB);
-    // }
 }
