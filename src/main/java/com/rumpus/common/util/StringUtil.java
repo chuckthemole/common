c57c8cbe378fd6acbd29050cfe61c86bc05d6d31
Chuck Thomas <chuckthemole@gmail.com>
2023-11-23 12:44:08 -0800

    Add some trimming helpers in stringutils

--- src/main/java/com/rumpus/common/util/StringUtil.java
+++ src/main/java/com/rumpus/common/util/StringUtil.java
@@ -1,6 +1,14 @@
 package com.rumpus.common.util;
 
-public class StringUtil {
+import com.rumpus.common.AbstractCommon;
+
+/**
+ * Feel free to implement static string helper methods here.
+ * If you are doing something repeatedly in your code, consider adding it here.
+ * 
+ * @brief String utilities
+ */
+public class StringUtil extends AbstractCommon {
     
     /**
      * @brief Checks if string is surrouned in given char
@@ -21,6 +29,88 @@ public class StringUtil {
      */
     public static boolean isSurrounded(String inpuString, char inputChar) {
         return StringUtil.isSurrounded(inpuString, inputChar, inputChar);
+    }
+
+    /**
+     * @brief remove surrounding chars
+     * 
+     * @return string without surrounding chars, if string is surrounded by given chars, otherwise returns original string
+     */
+    public static String trimStartAndEnd(String inputString, char startChar, char endChar) {
+        if(StringUtil.isSurrounded(inputString, startChar, endChar)) {
+            return inputString.substring(1, inputString.length() - 1);
+        }
+        LOG.info("String is not surrounded by given chars. Returning original string.");
+        return inputString;
+    }
+
+    /**
+     * @brief remove surrounding chars. Same as the overload but uses same char for beginning and end.
+     * 
+     * @param inputString string to trim
+     * @param inputChar char to trim
+     * @return string without surrounding chars, if string is surrounded by given char, otherwise returns original string
+     */
+    public static String trimStartAndEnd(String inputString, char inputChar) {
+        return StringUtil.trimStartAndEnd(inputString, inputChar, inputChar);
+    }
+
+    /**
+     * @brief remove beginning char
+     * 
+     * @return string without beginning char, if string starts with given char, otherwise returns original string
+     */
+    public static String trimBegin(String inputString, char inputChar) {
+        if(StringUtil.isStringNullOrEmpty(inputString)) {
+            return inputString;
+        }
+        if(inputString.charAt(0) != inputChar) {
+            LOG.info("Input string does not start with given char. Returning original string.");
+            return inputString;
+        }
+        return inputString.substring(1);
+    }
+
+    /**
+     * @brief remove end char
+     * @param inpuString string to trim
+     * @return string without end char, if string ends with given char, otherwise returns original string
+     */
+    public static String trimEnd(String inpuString, char inputChar) {
+        if(StringUtil.isStringNullOrEmpty(inpuString)) {
+            return inpuString;
+        }
+        if(inpuString.charAt(inpuString.length() - 1) != inputChar) {
+            LOG.info("Input string does not end with given char. Returning original string.");
+            return inpuString;
+        }
+        return inpuString.substring(0, inpuString.length() - 1);
+    }
+
+    /**
+     * Note: if you do NOT want to add a char to the beginning or end of the string, pass null for that char.
+     * 
+     * @brief add char to beginning and/or end of string
+     * 
+     * @param inpuString string to add char to
+     * @param startChar char to add to beginning of string
+     * @param endChar char to add to end of string
+     * @return string with given chars added to beginning and/or end of string
+     */
+    public static String addCharToStartAndOrEnd(String inpuString, Character startChar, Character endChar) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if(StringUtil.isStringNullOrEmpty(inpuString)) {
+            return inpuString;
+        }
+        if(startChar != null) {
+            stringBuilder.append(startChar).append(inpuString);
+        }
+        if(endChar != null) {
+            stringBuilder.append(endChar);
+        }
+        return stringBuilder.toString();
+
     }
 
     /**
@@ -52,5 +142,18 @@ public class StringUtil {
         StringBuilder sb = new StringBuilder();
         sb.append("\"").append(str).append("\"");
         return sb.toString();
+    }
+
+    /**
+     * @brief Checks if string is null or empty
+     * @param str string to check
+     * @return true if string is null or empty
+     */
+    public static boolean isStringNullOrEmpty(String str) {
+        if(str == null || str.length() == 0) {
+            LOG.info("Input string is null or empty.");
+            return true;
+        }
+        return false;
     }
 }
