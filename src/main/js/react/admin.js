206c685c912e648eca5385895ce0a5fcaabd7dd7
Chuck Thomas <chuckthemole@gmail.com>
2023-11-25 13:14:26 -0800

    Refactor base bath to consolidate to its own file, base_path

--- src/main/js/react/admin.js
+++ src/main/js/react/admin.js
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { getPathsFromBasePath, isCurrentUserAuthenticated, currentUserInfo, getCurrentBasePath, getCurrentUserAuthorities } from './common_requests';
+import { isCurrentUserAuthenticated, getCurrentUserAuthorities } from './common_requests';
+import BasePath from './base_path';
 
 export default function Admin() {
 
-    // TODO: this all seems like common code that should be in a common place. check out common_requests.js - chuck
-    const current_base_path = getCurrentBasePath(); // the current base path
-    const [base_path, setBasePath] = useState('/'); // the current base path as a state, default is '/'
-    const base_path_child_list = getPathsFromBasePath(base_path); // this list of paths accociated with the current base path
+    const base_path = BasePath();
     const [full_path_to_current_user_info, setFullPathCurrentUserInfo] = useState(''); // the current user info path
     const user_authorities = getCurrentUserAuthorities({get_user_auth_path: full_path_to_current_user_info}); // the current user info
     const is_user_authenticated = isCurrentUserAuthenticated(); // is the current user authenticated
 
-    useEffect(() => { // set the base path when the current_base_path hook changes
-        if(current_base_path.current_base_path !== undefined && current_base_path.current_base_path.path !== undefined) {
-            setBasePath(current_base_path.current_base_path.path);
-        }
-    }, [current_base_path]);
-
-    if(base_path_child_list !== undefined && base_path_child_list.common_paths !== undefined && base_path_child_list.common_paths.CurrentUserInfo !== undefined) {
-        if(full_path_to_current_user_info === '') {
-            setFullPathCurrentUserInfo(base_path + base_path_child_list.common_paths.CurrentUserInfo);
-        }
-        if(is_user_authenticated !== undefined && is_user_authenticated.isAuthenticated !== undefined) {
-            if(is_user_authenticated.isAuthenticated) {
-                if(user_authorities === undefined || user_authorities.isLoading || user_authorities.isError) {
-                    console.log('Error finding user authorities!');
-                } else {
-                    return user_authorities.includes('ROLE_ADMIN') ? <Link to={`/admin`} className="adminBtn button is-info"><strong>Admin</strong></Link> : <></>;
+    if(base_path.BASE_PATH_CHILD_LIST !== undefined) {
+        const base_path_child_list = base_path.BASE_PATH_CHILD_LIST;
+        if(base_path_child_list.common_paths !== undefined && base_path_child_list.common_paths.CurrentUserInfo !== undefined) {
+            if(full_path_to_current_user_info === '') {
+                setFullPathCurrentUserInfo(base_path.BASE_PATH + base_path_child_list.common_paths.CurrentUserInfo);
+            }
+            if(is_user_authenticated !== undefined && is_user_authenticated.isAuthenticated !== undefined) {
+                if(is_user_authenticated.isAuthenticated) {
+                    if(user_authorities === undefined || user_authorities.isLoading || user_authorities.isError) {
+                        console.log('Error finding user authorities!');
+                    } else {
+                        return user_authorities.includes('ROLE_ADMIN') ? <Link to={`/admin`} className="adminBtn button is-info"><strong>Admin</strong></Link> : <></>;
+                    }
                 }
             }
         }
--- src/main/js/react/user_icon.js
+++ src/main/js/react/user_icon.js
@@ -3,24 +3,24 @@ import { Tooltip as ReactTooltip } from "react-tooltip";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationTriangle, faUser } from '@fortawesome/free-solid-svg-icons';
 import { getPathsFromBasePath, isCurrentUserAuthenticated, currentUserInfo, getCurrentBasePath } from './common_requests';
+import BasePath from './base_path';
 
 export default function UserIcon() {
-    const current_base_path = getCurrentBasePath(); // the current base path
-    const [base_path, setBasePath] = useState('/'); // the current base path as a state, default is '/'
-    const base_path_child_list = getPathsFromBasePath(base_path); // this list of paths accociated with the current base path
+
+    const base_path = BasePath();
     const [base_path_current_user_info, setBasePathCurrentUserInfo] = useState(''); // the current user info path
     const user_info = currentUserInfo({get_user_info_path: base_path_current_user_info}); // the current user info
     const is_user_authenticated = isCurrentUserAuthenticated(); // is the current user authenticated
 
-    useEffect(() => { // set the base path when the current_base_path hook changes
-        if(current_base_path.current_base_path !== undefined && current_base_path.current_base_path.path !== undefined) {
-            setBasePath(current_base_path.current_base_path.path);
-        }
-    }, [current_base_path]);
-
     if(!is_user_authenticated.isAuthenticated) { // no icon to display if user is not authenticated
         return (<></>);
-    } else if(base_path_child_list === undefined || base_path_child_list.common_paths === undefined || base_path_child_list.common_paths.CurrentUserInfo === undefined) {
+    } else if(
+        base_path === undefined ||
+        base_path.BASE_PATH === undefined ||
+        base_path.BASE_PATH_CHILD_LIST === undefined ||
+        base_path.BASE_PATH_CHILD_LIST.common_paths === undefined ||
+        base_path.BASE_PATH_CHILD_LIST.common_paths.CurrentUserInfo === undefined) {
+
         console.log('Error finding user icon!');
         return(
             <>
@@ -39,8 +39,7 @@ export default function UserIcon() {
     } else {
 
         if(base_path_current_user_info === '') {
-            // console.log("Setting base_path_current_user_info: " + base_path + base_path_child_list.common_paths.CurrentUserInfo);
-            setBasePathCurrentUserInfo(base_path + base_path_child_list.common_paths.CurrentUserInfo);
+            setBasePathCurrentUserInfo(base_path.BASE_PATH + base_path.BASE_PATH_CHILD_LIST.common_paths.CurrentUserInfo);
         }
 
         if (user_info.isError) {
